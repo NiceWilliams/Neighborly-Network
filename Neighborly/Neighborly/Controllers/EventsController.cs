2660f9cce6c7cebbe70e6b4643d007996fe37c3c
AaronW <aaronwhildkhild@yahoo.com>
2022-01-21 01:07:50 -0600

    created EventModel + EventController + index add action + detail action + event/index view + event/add view + event/detail view

---
+++ Neighborly/Neighborly/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neighborly.Models;
+using System.Collections.Generic;
+
+namespace Neighborly.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<Event> Events = new List<Event>();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(new Event(name,desc));
+
+            return Redirect("/Events");
+        }
+
+        [HttpGet]
+        [Route("/Events/Detail")]
+        public IActionResult Detail()
+        {
+            return View();
+        } 
+    }
+}
